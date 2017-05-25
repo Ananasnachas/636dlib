c398a1fd0d0ec77e7df691be0d998a0aed432347
Ananasnachas <nikita_maslov_warspear@mail.ru>
2017-05-25 18:51:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,51 @@
 /* main.c */
 #include "matr.h"
-
-void read_matr(double **m, int n)
-{
-/* implement the reading n*n numbers from stdin to 2-dim array */
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 int main()
 {
-	int n;
-	double **a;
-	double **b;
-/* main for tests: alloc mem for 2 square matrices and read them from stdin, then run 'matr()' */
-/* input format (stdin): n
- then 2 square matrices (n * n)
-*/	
-	matr(a,b,n);
+	int n, i, j;
+	FILE *in, *out;
+	double **a, **b, **c;
+	in = fopen("in.txt", "r");
+	out = fopen("out.txt", "w");
+	fscanf(in, "%d", &n);
+	a = (double**)malloc(n*sizeof(double*));
+	b = (double**)malloc(n*sizeof(double*));
+	for (i = 0; i < n; i++)
+	{
+		a[i] = (double*)malloc(n*sizeof(double));
+		for (j = 0; j < n; j++)
+			fscanf(in, "%lf", &(a[i][j]));
+	}
+	for (i = 0; i < n; i++)
+	{
+		b[i] = (double*)malloc(n*sizeof(double));
+		for (j = 0; j < n; j++)
+			fscanf(in, "%lf", &(b[i][j]));
+	}
+	int st = clock();
+	c = matr(a, b, n);
+	int end = clock();
+	printf("%d ", end - st);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			fprintf(out, "%lf ", c[i][j]);
+		}
+		fprintf(out, "\n");
+		free(c[i]);
+		free(a[i]);
+		free(b[i]);
+	}
+	free(a);
+	free(b);
+	free(c);
+	fclose(out);
+	fclose(in);
 	return 0;
 }
